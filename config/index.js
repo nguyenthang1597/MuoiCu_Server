c8e27c7cf1a0760ab63d3cf79e67d051b987f2df
Nguyen Hieu Thang <nguyenthang1597@gmail.com>
2019-03-29 10:55:51 +0700

    setting db

---
+++ config/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    database: {
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE,
+        host: process.env.HOST,
+        limit: process.env.LIMIT
+    }
+}
