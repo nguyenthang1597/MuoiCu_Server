ba26a4dca76bbddd3263ea502a43396dc22f88fa
Nguyen Kim Thao <taonuaa004@gmail.com>
2019-03-30 12:55:40 +0700

    sua lai item them maphutung

--- routes/Item.js
+++ routes/Item.js
@@ -3,9 +3,12 @@ const Item = require('../controllers/Item');
 
 router.get('/', Item.getList);
 router.get('/:ma', Item.getByMa);
+router.get('/:maphutung', Item.getByMa);
 router.post('/', Item.add);
 router.put('/:ma', Item.update);
+router.put('/:maphutung', Item.update);
 router.delete('/:ma', Item.delete);
+router.delete('/:maphutung', Item.delete);
 
 
 module.exports = router;
