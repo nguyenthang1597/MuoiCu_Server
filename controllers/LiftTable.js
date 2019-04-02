86bd0e4a06158c9ee128458d1c566be7f6391ac7
Nguyen Kim Thao <taonuaa004@gmail.com>
2019-04-03 00:45:17 +0700

    thanhtoan

--- controllers/LiftTable.js
+++ controllers/LiftTable.js
@@ -1,17 +1,20 @@
-var liftTable = new Array(8).fill(false);
+var liftTable = new Array(8).fill({ "trangthai": false, "mahoadon": "" });
 
 module.exports = function (io) {
     io.on('connection', function (socket) {
         socket.emit('connected', liftTable);
         socket.on('select', (data) => {
-            let index = Number.parseInt(data);
-            liftTable[data] = true;
+            data = JSON.parse(data);
+            let index = Number.parseInt(data.maban);
+            liftTable[index].trangthai = true;
+            liftTable[index].mahoadon = data.mahoadon;
             socket.emit('lifttable', liftTable);
             socket.broadcast.emit('lifttable', liftTable);
         })
         socket.on('release', (data) => {
             let index = Number.parseInt(data);
-            liftTable[data] = false;
+            liftTable[index].trangthai = true;
+            liftTable[index].mahoadon = "";
             socket.emit('lifttable', liftTable);
             socket.broadcast.emit('lifttable', liftTable);
         })
