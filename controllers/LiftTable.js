df3fe61fe3228cb3fb72aa7ca8dd7f9f8d2b9303
Nguyen Kim Thao <taonuaa004@gmail.com>
2019-04-03 18:56:41 +0700

    heelo

--- controllers/LiftTable.js
+++ controllers/LiftTable.js
@@ -3,18 +3,27 @@ module.exports = function (io) {
     io.on('connection', function (socket) {
         socket.emit('connected', liftTable);
         socket.on('select', (data) => {
-            let index = Number.parseInt(data.maban);
-            liftTable[index].trangthai = true;
-            liftTable[index].mahoadon = data.mahoadon;
-            socket.emit('lifttable', liftTable);
-            socket.broadcast.emit('lifttable', liftTable);
+            try {
+                let index = Number.parseInt(data.maban);
+                liftTable[index].trangthai = true;
+                liftTable[index].mahoadon = data.mahoadon;
+                socket.emit('lifttable', liftTable);
+                socket.broadcast.emit('lifttable', liftTable);
+            } catch (e) {
+
+            }
+
         })
         socket.on('release', (data) => {
-            let index = Number.parseInt(data.maban);
-            liftTable[index].trangthai = false;
-            liftTable[index].mahoadon = "";
-            socket.emit('lifttable', liftTable);
-            socket.broadcast.emit('lifttable', liftTable);
+            try {
+                let index = Number.parseInt(data.maban);
+                liftTable[index].trangthai = false;
+                liftTable[index].mahoadon = "";
+                socket.emit('lifttable', liftTable);
+                socket.broadcast.emit('lifttable', liftTable);
+            } catch (e) {
+
+            }
         })
     })
 }
