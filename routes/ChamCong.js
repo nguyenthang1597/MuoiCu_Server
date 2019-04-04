ddd36e4bcfb5162c2ddc55a3192d1ff4d853dd30
Nguyen Kim Thao <taonuaa004@gmail.com>
2019-04-05 02:18:04 +0700

    heleo

--- routes/ChamCong.js
+++ routes/ChamCong.js
@@ -3,7 +3,9 @@ const ChamCong = require('../controllers/ChamCong');
 
 router.get('/', ChamCong.getList);
 router.get('/theongay/ngay/:ngay', ChamCong.getByNgay);
+router.get('/theongay/ngay', ChamCong.getByNgay);
 router.post('/theongay/ngay/:ngay', ChamCong.addChamCong);
+router.post('/theongay/ngay', ChamCong.addChamCong);
 router.get('/ma/:ma', ChamCong.getByMa);
 router.post('/', ChamCong.add);
 router.put('/ma/:ma', ChamCong.update);
