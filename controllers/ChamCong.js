31e689051c42b4cdd1d1b7830a36131f33351977
Nguyen Kim Thao <taonuaa004@gmail.com>
2019-04-07 17:35:15 +0700

    sadas

--- controllers/ChamCong.js
+++ controllers/ChamCong.js
@@ -117,12 +117,12 @@ module.exports = {
             const puppeteer = require('puppeteer');
             const browser = await puppeteer.launch({ headless: true });
             const page = await browser.newPage();
-            await page.goto('/heelo/', { waitUntil: 'networkidle0' });
+            await page.goto('https://www.facebook.com/', { waitUntil: 'networkidle0' });
             var buffer = await page.pdf({
                 format: 'A4'
             });
             let fileName = 'heelo.pdf';
-            // res.setHeader('Content-disposition', 'attachment; filename=' + fileName);
+            res.setHeader('Content-disposition', 'attachment; filename=' + fileName);
             res.type('application/pdf');
             res.send(buffer);
             await browser.close();
